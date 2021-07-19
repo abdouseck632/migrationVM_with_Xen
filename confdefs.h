50cabdffe6192aecac1e89f969bdd2a2dafb35ff
abdouseck632 <abdouseck632@gmail.com>
2021-07-19 15:00:58 +0000

    second commit

---
+++ confdefs.h
@@ -0,0 +1,7 @@
+/* confdefs.h */
+#define PACKAGE_NAME "Xen Hypervisor"
+#define PACKAGE_TARNAME "xen"
+#define PACKAGE_VERSION "4.15"
+#define PACKAGE_STRING "Xen Hypervisor 4.15"
+#define PACKAGE_BUGREPORT "xen-devel@lists.xen.org"
+#define PACKAGE_URL "https://www.xen.org/"
